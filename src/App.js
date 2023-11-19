180ce95cf8c9292e8c408151e9de186182e1b6dd
Faisal Mujtaba Saleem <faisalmujtaba2005@gmail.com>
2023-11-19 15:31:26 +0500

    Video. Creating Navbar and Routes | Complete React Course in Hindi #56 Completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>iNotebook</h1>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
